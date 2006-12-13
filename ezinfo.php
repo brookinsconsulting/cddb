228ce9efb5e566ed127c47f1bf32ff59a52723e9
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-13 19:06:44 +0000

    3.9 extension info file

---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class cddbInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "CDDB",
+            'Version' => "0.x",
+            'Copyright' => "Copyright (C) 2006 Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
